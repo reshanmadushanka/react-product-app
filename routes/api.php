7b00f6d5b6ab8d6ab30a444afbe972550417fe59
reshanmadushanka <reshanmadushanka@gmail.com>
2024-06-30 16:35:48 +0530

    complete user authentication with passport

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\Api\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
